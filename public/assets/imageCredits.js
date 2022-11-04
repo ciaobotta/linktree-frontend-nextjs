723d28bfe1b7bf047766c487111f27b5a4c475e5
ciaobotta <69284419+ciaobotta@users.noreply.github.com>
2022-11-04 09:35:27 -0400

    first commit of linktree

---
+++ public/assets/imageCredits.js
@@ -0,0 +1,10 @@
+// image credits
+// pexels.com:
+
+/**
+ * PEXELS.COM
+photo: profile pic
+ link: https://www.pexels.com/photo/woman-wearing-bikini-on-sea-shore-428549/
+ photographer: Spencer Selover
+pexelsProfile: https://www.pexels.com/@spencer-selover-142259/
+ */
